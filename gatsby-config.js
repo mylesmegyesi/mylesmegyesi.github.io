35281cecceaafbd5159c3076b6cd51a570ac0bca
Myles Megyesi <mylesmegyesi@users.noreply.github.com>
2018-11-11 21:35:48 +0000

    Adds CSS reset and default body font

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
+    "gatsby-plugin-glamor",
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-typescript"
+    "gatsby-plugin-typescript",
   ],
 }
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,6 +11,15 @@ export const Layout = ({ children }: LayoutProps) => (
       title="Myles Megyesi"
     >
       <html lang="en" />
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" />
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css" rel="stylesheet" />
+
+      {/*For some reason Glamor doesn't work within react-helment when JavaScript is disabled*/}
+      <style type="text/css">{`
+        body {
+          font-family: 'Open Sans', sans-serif;
+        }
+    `}</style>
     </Helmet>
     <div>
       {children}
